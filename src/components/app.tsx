a6ce2b29bf44c3370f14e272d4e9f9d115a73575
Sergey Chirkov <chsergey1003@gmail.com>
2024-09-29 14:16:02 +0300

    first commit

---
+++ src/components/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>StudX</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import "./main.css";
+
+import App from "./components/app.tsx";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import {
+  BrowserRouter
+} from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
